ed225aa0dd37fa1c51dc7b0be1182b941afaaee4
KhalidHosein <66786796+KhalidHosein@users.noreply.github.com>
2020-10-01 14:26:47 -0400

    Added Date range picker component

--- src/Banner.js
+++ src/Banner.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Banner.css";
+import Search from "./Search";
 import { Button } from "@material-ui/core";
 function Banner() {
+  const [showSearch, setshowSearch] = useState(false);
   return (
     <div className="banner">
+      <div className="banner_search">
+        {showSearch && <Search />}
+        <Button
+          onClick={() => setshowSearch(!showSearch)}
+          className="banner_searchButton"
+          variant="outline"
+        >
+          Search Dates
+        </Button>
+      </div>
       <div className="banner_info">
         <h1>Get out and stretch your imagination</h1>
         <h5>
